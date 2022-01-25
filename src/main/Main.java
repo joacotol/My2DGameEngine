cb97936e4f0666bb8a158b22c2e795c9b99c5df4
joacotol <joacochrist@gmail.com>
2022-01-25 18:35:56 -0500

    First commit in repo

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creates a window using JFrame
+        JFrame window = new JFrame();
+        // This lets the window properly close when user clicks the close ('x') button
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
